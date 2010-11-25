2b652de00f6abb84d98028c9f45304ebc39cc081
Mike Douglas <mike.douglas@gmail.com>
2010-11-24 17:10:58 -0800

    Word count utility.

---
+++ utilities/wc.el
@@ -0,0 +1,28 @@
+(defun count-words (start end)
+  "Print number of words in the region."
+  (interactive "r")
+  (save-excursion
+    (save-restriction
+      (narrow-to-region start end)
+      (goto-char (point-min))
+      (count-matches "\\sw+"))))
+
+(defun wc-buffer ()
+  "Display the number of words in a buffer."
+  (interactive)
+  (message (concat "The current buffer contains "
+                   (number-to-string
+                    (count-words (point-min) (point-max)))
+                   " words.")))
+
+(defun wc-region (start end)
+  "Display the number of words in a region."
+  (interactive "r")
+  (message (concat "The current region contains "
+                   (number-to-string
+                    (count-words start end))
+                   " words.")))
+
+(defalias 'wc 'wc-buffer)
+
+(provide 'wc)
